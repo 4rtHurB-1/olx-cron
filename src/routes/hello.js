19e87dd565c1c7de7cc272d9609a2335a05c8a9d
4rtHurB <arturborikun2@gmail.com>
2021-01-11 19:57:14 +0200

    add olx req

--- src/routes/hello.js
+++ src/routes/hello.js
@@ -1,10 +1,11 @@
 import express from 'express';
-import request from 'request';
-
+import axios from "axios";
 import {getConfigValue} from "../utils";
-const router = express.Router();
 
 import AdvertList from "../services/adverts-list";
+
+const router = express.Router();
+const USER_AGENT = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/33.0.1750.117 Safari/537.36';
 
 router.get('/', (req, res) => {
     res.send(200, 'It works :)');
@@ -27,22 +28,16 @@ router.get('/req-to-olx', async (req, res) => {
     } else if(!req.query.url) {
         res.status(400).json({error: `Param 'url' is required`});
     } else {
-        let options = {
-            method: 'GET',
-            url: req.query.url,
-            headers: {
-                'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/33.0.1750.117 Safari/537.36'
-            }
-        };
-
-        request(options, function (error, response, body) {
-            if (!error) {
-                console.log(body);
-                res.status(200).send(body);
-            } else {
-                console.log(error);
-            }
-        });
+        try {
+            let result = await axios({
+                method: 'GET',
+                url: req.query.url,
+                headers: {'User-Agent': USER_AGENT}
+            });
+            res.status(result.status).send(result.data);
+        } catch (e) {
+            console.log(e.message);
+        }
     }
 })
 
